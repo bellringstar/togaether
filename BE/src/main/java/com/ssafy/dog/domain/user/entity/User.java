edef4645d2c53c04bfb92fa224debd0bb14245a4
Phabala <kihong2424@gmail.com>
2023-10-23 01:06:24 +0900

    [BE] Feat : User 엔티티 추가

--- BE/src/main/java/com/ssafy/dog/domain/user/entity/User.java
+++ BE/src/main/java/com/ssafy/dog/domain/user/entity/User.java
@@ -1,48 +1,34 @@
 package com.ssafy.dog.domain.user.entity;
 
-import com.ssafy.dog.domain.user.model.Gender;
-
+import com.ssafy.dog.domain.board.entity.Board;
+import lombok.Data;
 import javax.persistence.*;
 import java.time.LocalDateTime;
+import java.util.List;
 
+@Data
 @Entity
-@Table(name = "user")
+@Table(name = "User")
 public class User {
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "user_id")
-    private Long userId;
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long userId;
 
-    @Column(name = "user_login_id", length = 11, nullable = false)
-    private String userLoginId;
+  private String userPw;
+  private String userNickname;
+  private LocalDateTime userCreatedAt;
+  private LocalDateTime userUpdatedAt;
+  private String userAboutMe;
+  private String userGender;
+  private Boolean userTermsAgreed;
 
-    @Column(name = "user_pw", length = 256, nullable = false)
-    private String userPw;
+  @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+  private List<Follow> follows;
 
-    @Column(name = "user_nickname", length = 30, nullable = false)
-    private String userNickname;
+  @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+  private List<Dog> dogs;
 
-    @Lob
-    @Column(name = "user_picture")
-    private String userPicture;
-
-    @Column(name = "user_created_at", nullable = false)
-    private LocalDateTime userCreatedAt;
-
-    @Column(name = "user_updated_at", nullable = false)
-    private LocalDateTime userUpdatedAt;
-
-    @Column(name = "user_about_me", length = 200)
-    private String userAboutMe;
-
-    @Enumerated(EnumType.STRING)
-    @Column(name = "user_gender", length = 1)
-    private Gender userGender;
-
-    @Column(name = "user_terms_agreed", nullable = false)
-    private Boolean userTermsAgreed;
-
-    // Getter, Setter, Constructors, and other methods
-
+  @OneToMany(mappedBy = "user")
+  private List<Board> boards;
 }
