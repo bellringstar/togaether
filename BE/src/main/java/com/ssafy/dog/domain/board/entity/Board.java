140bcce376d439ef09dd8a17bed8612b83fe1814
이성연 <chamsy39@gmail.com>
2023-10-25 09:48:15 +0900

    [BE] Feat : Board 관련 Entity 생성

---
+++ BE/src/main/java/com/ssafy/dog/domain/board/entity/Board.java
@@ -0,0 +1,43 @@
+package com.ssafy.dog.domain.board.entity;
+
+import java.math.BigInteger;
+import java.time.LocalDateTime;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "Board")
+public class Board {
+	@Id
+	@Column(name = "board_id")
+	private BigInteger id;
+	@Column(name = "user_id")
+	private BigInteger userId;
+	@Column(name = "board_title")
+	private String title;
+	@Column(name = "board_media_type")
+	@Enumerated(value = EnumType.STRING)
+	private MediaType mediaType;
+
+	@Column(name = "board_content")
+	private String content;
+
+	@Column(name = "board_likes")
+	private int likes;
+
+	@Column(name = "board_created_at")
+	private LocalDateTime created;
+
+	@Column(name = "board_updated_at")
+	private LocalDateTime updated;
+
+	@Column(name = "board_scope")
+	@Enumerated(value = EnumType.STRING)
+	private Scope scope;
+
+}
---
+++ BE/src/main/java/com/ssafy/dog/domain/board/entity/Scope.java
@@ -0,0 +1,5 @@
+package com.ssafy.dog.domain.board.entity;
+
+public enum Scope {
+	Everyone, Friends
+}
