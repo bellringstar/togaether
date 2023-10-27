c47ef0203425603a43a98c5ee5ef6113b01fd99a
Phabala(Kim Gihong) <kihong2424@gmail.com>
2023-10-27 12:13:47 +0900

    [BE] Feat : Jwt Valid URI 추가

---
+++ BE/src/main/java/com/ssafy/dog/domain/user/controller/JwtController.java
@@ -0,0 +1,16 @@
+package com.ssafy.dog.domain.user.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.ssafy.dog.common.api.Api;
+
+@RequestMapping("/jwt")
+@RestController
+public class JwtController {
+	@GetMapping("/valid")
+	public Api<Object> valid(String jwt) {
+		return Api.ok("JWT Valid 완료");
+	}
+}
