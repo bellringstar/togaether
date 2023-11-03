334d8ffa81cbe1e8ca0783f0306a8b1eb9ac3c30
김대홍 <rlaeoghd1885@naver.com>
2023-11-03 17:49:10 +0900

    [BE] Feat : kafka sendnotice Config 추가

--- BE/src/main/java/com/ssafy/dog/domain/chat/service/KafkaConsumerService.java
+++ BE/src/main/java/com/ssafy/dog/domain/chat/service/KafkaConsumerService.java
@@ -7,6 +7,7 @@ import org.springframework.messaging.simp.SimpMessageSendingOperations;
 import org.springframework.stereotype.Service;
 
 import com.ssafy.dog.domain.chat.dto.MessageDto;
+import com.ssafy.dog.domain.chat.dto.NoticeDto;
 import com.ssafy.dog.domain.chat.util.KafkaConstants;
 
 import lombok.RequiredArgsConstructor;
@@ -19,8 +20,8 @@ public class KafkaConsumerService {
 
 	private final SimpMessageSendingOperations template;
 
-	@KafkaListener(topics = KafkaConstants.KAFKA_TOPIC, groupId = KafkaConstants.GROUP_ID)
-	public void receive(MessageDto message) throws IOException {
+	@KafkaListener(topics = KafkaConstants.KAFKA_CHAT_TOPIC, groupId = KafkaConstants.GROUP_ID)
+	public void receiveChat(MessageDto message) throws IOException {
 
 		// log.info("Consumed Message : " + message.getMessage());
 		// HashMap<String, String> msg = new HashMap<>();
@@ -37,5 +38,25 @@ public class KafkaConsumerService {
 		// /sub/chatroom/{roomId} - 구독
 		// 메시지객체 내부의 채팅방번호를 참조하여, 해당 채팅방 구독자에게 메시지를 발송한다.
 		template.convertAndSend("/sub/chatroom/" + message.getRoomId(), message);
+	}
+
+	@KafkaListener(topics = KafkaConstants.KAFKA_NOTICE_TOPIC, groupId = KafkaConstants.GROUP_ID)
+	public void receiveNotice(NoticeDto notice) throws IOException {
+
+		// log.info("Consumed Message : " + message.getMessage());
+		// HashMap<String, String> msg = new HashMap<>();
+		// msg.put("roomId", String.valueOf(message.getRoomId()));
+		// msg.put("message", message.getMessage());
+		// msg.put("writer", message.getWriter());
+		//
+		// ObjectMapper mapper = new ObjectMapper();
+		// template.convertAndSend("/topic/tt", mapper.writeValueAsString(msg));
+
+		log.info("전송 위치 = /sub/notice/" + notice.getRoomId());
+		// log.info("채팅 방으로 메시지 전송 = {}", message.toString());
+
+		// /sub/chatroom/{roomId} - 구독
+		// 메시지객체 내부의 채팅방번호를 참조하여, 해당 채팅방 구독자에게 메시지를 발송한다.
+		template.convertAndSend("/sub/notice/" + notice.getRoomId(), notice);
 	}
 }
