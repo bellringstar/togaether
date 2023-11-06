08aa22e5f77e2c07e19d2284d8de505c5ed036b0
Phabala(Kim Gihong) <kihong2424@gmail.com>
2023-11-06 11:45:14 +0900

    [BE] Chore : 403 ìError Added

--- BE/src/main/java/com/ssafy/dog/common/error/ErrorCode.java
+++ BE/src/main/java/com/ssafy/dog/common/error/ErrorCode.java
@@ -12,6 +12,7 @@ public enum ErrorCode implements ErrorCodeIfs {
 	OK(200, 200, "성공"),
 	BAD_REQUEST(HttpStatus.BAD_REQUEST.value(), 400, "잘못된 요청"),
 	SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR.value(), 500, "서버에러"),
+	FORBIDDEN(HttpStatus.FORBIDDEN.value(), 403, "Access Denied"),
 	NULL_POINT(HttpStatus.INTERNAL_SERVER_ERROR.value(), 512, "Null point"),
 	VALIDATION_ERROR(HttpStatus.INTERNAL_SERVER_ERROR.value(), 512, "테스트용");
 
