6e601ea37db75ce526b76b639819ee0a9aa2210e
Phabala(Kim Gihong) <kihong2424@gmail.com>
2023-11-06 10:29:14 +0900

    [BE] Refactor : User DTO 디렉토리 구조 리팩토링

--- BE/src/main/java/com/ssafy/dog/domain/user/dto/request/UserLoginReq.java
+++ BE/src/main/java/com/ssafy/dog/domain/user/dto/request/UserLoginReq.java
@@ -7,7 +7,7 @@ import javax.validation.constraints.Pattern;
 import lombok.Getter;
 
 @Getter
-public class UserLoginRequestDto {
+public class UserLoginReq {
 	@NotBlank(message = "이메일(아이디)은 필수 입력 값입니다.")
 	@Email(message = "로그인 아이디는 이메일 형식이어야 합니다.",
 		regexp = "^[\\w-\\.]+@([\\w-]+\\.)+[\\w-]{2,4}$")
