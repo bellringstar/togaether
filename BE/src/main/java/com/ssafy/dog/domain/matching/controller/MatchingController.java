0fa69f2dae6b039ee306b6d1d0055b7278388d30
김현종 <dw3510@naver.com>
2023-11-06 11:44:28 +0900

    [BE] feat : matching을 위한 컨트롤러 작성

---
+++ BE/src/main/java/com/ssafy/dog/domain/matching/controller/MatchingController.java
@@ -0,0 +1,30 @@
+package com.ssafy.dog.domain.matching.controller;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.ssafy.dog.common.api.Api;
+import com.ssafy.dog.domain.matching.dto.MatchingUserResponse;
+import com.ssafy.dog.domain.matching.service.MatchingService;
+
+import lombok.RequiredArgsConstructor;
+
+@RequestMapping("/api")
+@RestController
+@RequiredArgsConstructor
+public class MatchingController {
+
+	private final MatchingService matchingService;
+
+	@GetMapping("/matching")
+	public Api<List<MatchingUserResponse>> matchingUsers(String userId) {
+		return Api.ok(matchingService.matchingUser(userId)
+			.stream()
+			.map(MatchingUserResponse::toResponse)
+			.collect(Collectors.toList()));
+	}
+}
