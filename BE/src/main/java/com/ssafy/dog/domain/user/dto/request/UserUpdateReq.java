07ca3e90e34dd33882d02b2a1576fb5614627e37
Phabala(Kim Gihong) <kihong2424@gmail.com>
2023-11-07 14:23:32 +0900

    [BE] Chore : 유저 업데이트 patch로 변경, jwt/valid return 값 단순 숫자로 변경

--- BE/src/main/java/com/ssafy/dog/domain/user/dto/request/UserUpdateReq.java
+++ BE/src/main/java/com/ssafy/dog/domain/user/dto/request/UserUpdateReq.java
@@ -1,17 +1,33 @@
 package com.ssafy.dog.domain.user.dto.request;
 
 import com.ssafy.dog.domain.user.model.UserGender;
-
 import lombok.Getter;
+
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.Lob;
+import javax.validation.constraints.Size;
 
 @Getter
 public class UserUpdateReq {
-	private String userNickname;
-	private String userPhone;
-	private String userPicture;
-	private String userAboutMe;
-	private UserGender userGender;
-	private Double userLatitude;
-	private Double userLongitude;
-	private String userAddress;
+    private String userNickname;
+
+    @Size(min = 10, max = 11, message = "핸드폰 번호는 10글자 이상 11글자 이하여야 합니다.")
+    private String userPhone;
+
+    @Lob
+    private String userPicture;
+
+    @Size(max = 200, message = "자기소개는 200글자 이하여야 합니다.")
+    private String userAboutMe;
+
+    @Enumerated(EnumType.STRING)
+    private UserGender userGender;
+
+    private Double userLatitude;
+
+    private Double userLongitude;
+
+    @Size(max = 255, message = "주소는 255자 이하여야 합니다.")
+    private String userAddress;
 }
--- BE/src/main/java/com/ssafy/dog/domain/user/service/UserServiceImpl.java
+++ BE/src/main/java/com/ssafy/dog/domain/user/service/UserServiceImpl.java
@@ -1,9 +1,21 @@
 package com.ssafy.dog.domain.user.service;
 
-import java.util.Collections;
-import java.util.List;
-import java.util.regex.Pattern;
-
+import com.ssafy.dog.common.api.Api;
+import com.ssafy.dog.common.error.UserErrorCode;
+import com.ssafy.dog.common.exception.ApiException;
+import com.ssafy.dog.domain.user.dto.request.UserLoginReq;
+import com.ssafy.dog.domain.user.dto.request.UserSignupReq;
+import com.ssafy.dog.domain.user.dto.request.UserUpdateReq;
+import com.ssafy.dog.domain.user.dto.response.UserLoginRes;
+import com.ssafy.dog.domain.user.dto.response.UserReadRes;
+import com.ssafy.dog.domain.user.dto.response.UserUpdateRes;
+import com.ssafy.dog.domain.user.entity.User;
+import com.ssafy.dog.domain.user.model.UserRole;
+import com.ssafy.dog.domain.user.repository.UserRepository;
+import com.ssafy.dog.security.JwtToken;
+import com.ssafy.dog.security.JwtTokenProvider;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.GrantedAuthority;
@@ -12,22 +24,9 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import com.ssafy.dog.common.api.Api;
-import com.ssafy.dog.common.error.UserErrorCode;
-import com.ssafy.dog.common.exception.ApiException;
-import com.ssafy.dog.domain.user.dto.request.UserLoginReq;
-import com.ssafy.dog.domain.user.dto.request.UserSignupReq;
-import com.ssafy.dog.domain.user.dto.request.UserUpdateReq;
-import com.ssafy.dog.domain.user.dto.response.UserLoginRes;
-import com.ssafy.dog.domain.user.dto.response.UserUpdateRes;
-import com.ssafy.dog.domain.user.entity.User;
-import com.ssafy.dog.domain.user.model.UserRole;
-import com.ssafy.dog.domain.user.repository.UserRepository;
-import com.ssafy.dog.security.JwtToken;
-import com.ssafy.dog.security.JwtTokenProvider;
-
-import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
+import java.util.Collections;
+import java.util.List;
+import java.util.regex.Pattern;
 
 @Slf4j
 @RequiredArgsConstructor
@@ -35,119 +34,128 @@ import lombok.extern.slf4j.Slf4j;
 @Service
 public class UserServiceImpl implements UserService {
 
-	private final UserRepository userRepository;
-	private final PasswordEncoder passwordEncoder;
-	private final JwtTokenProvider jwtTokenProvider;
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+    private final JwtTokenProvider jwtTokenProvider;
 
-	private static final Pattern EMAIL_PATTERN = Pattern.compile("^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,6}$");
-	private static final Pattern PASSWORD_PATTERN = Pattern.compile(
-		"^(?=.*[A-Z])(?=.*[A-Za-z])(?=.*\\d)(?=.*[@$!%*#?&])[A-Za-z\\d@$!%*#?&]{8,16}$");
+    private static final Pattern EMAIL_PATTERN = Pattern.compile("^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,6}$");
+    private static final Pattern PASSWORD_PATTERN = Pattern.compile(
+            "^(?=.*[A-Z])(?=.*[A-Za-z])(?=.*\\d)(?=.*[@$!%*#?&])[A-Za-z\\d@$!%*#?&]{8,16}$");
 
-	@Transactional
-	@Override
-	public Api<String> create(UserSignupReq userSignupReq) {
+    @Transactional
+    @Override
+    public Api<String> create(UserSignupReq userSignupReq) {
 
-		if (!isValidEmail(userSignupReq.getUserLoginId())) {
-			throw new ApiException(UserErrorCode.INVALID_EMAIL);
-		}
+        if (!isValidEmail(userSignupReq.getUserLoginId())) {
+            throw new ApiException(UserErrorCode.INVALID_EMAIL);
+        }
 
-		if (!isValidPassword(userSignupReq.getUserPw1())) {
-			throw new ApiException(UserErrorCode.INVALID_PASSWORD);
-		}
+        if (!isValidPassword(userSignupReq.getUserPw1())) {
+            throw new ApiException(UserErrorCode.INVALID_PASSWORD);
+        }
 
-		if (userRepository.findByUserLoginId(userSignupReq.getUserLoginId()).isPresent()) {
-			throw new ApiException(UserErrorCode.EMAIL_EXISTS);
-		}
+        if (userRepository.findByUserLoginId(userSignupReq.getUserLoginId()).isPresent()) {
+            throw new ApiException(UserErrorCode.EMAIL_EXISTS);
+        }
 
-		if (userRepository.findByUserNickname(userSignupReq.getUserNickname()).isPresent()) {
-			throw new ApiException(UserErrorCode.NICKNAME_EXISTS);
-		}
+        if (userRepository.findUserByUserNickname(userSignupReq.getUserNickname()).isPresent()) {
+            throw new ApiException(UserErrorCode.NICKNAME_EXISTS);
+        }
 
-		if (!userSignupReq.getUserTermsAgreed().equals(true)) {
-			throw new ApiException(UserErrorCode.TERMS_NOT_AGREED);
-		}
+        if (!userSignupReq.getUserTermsAgreed().equals(true)) {
+            throw new ApiException(UserErrorCode.TERMS_NOT_AGREED);
+        }
 
-		if (userRepository.findByUserPhone(userSignupReq.getUserPhone()).isPresent()) {
-			throw new ApiException(UserErrorCode.PHONE_EXISTS);
-		}
+        if (userRepository.findByUserPhone(userSignupReq.getUserPhone()).isPresent()) {
+            throw new ApiException(UserErrorCode.PHONE_EXISTS);
+        }
 
-		String encodedPassword = passwordEncoder.encode(userSignupReq.getUserPw1());
-		User user = User.UserBuilder.anUser()
-			.withUserLoginId(userSignupReq.getUserLoginId())
-			.withUserPw(encodedPassword)
-			.withUserNickname(userSignupReq.getUserNickname())
-			.withUserPhone(userSignupReq.getUserPhone())
-			.withUserTermsAgreed(userSignupReq.getUserTermsAgreed())
-			.withUserIsRemoved(false)
-			.withUserRole(UserRole.USER)
-			.build();
+        String encodedPassword = passwordEncoder.encode(userSignupReq.getUserPw1());
+        User user = User.UserBuilder.anUser()
+                .withUserLoginId(userSignupReq.getUserLoginId())
+                .withUserPw(encodedPassword)
+                .withUserNickname(userSignupReq.getUserNickname())
+                .withUserPhone(userSignupReq.getUserPhone())
+                .withUserTermsAgreed(userSignupReq.getUserTermsAgreed())
+                .withUserIsRemoved(false)
+                .withUserRole(UserRole.USER)
+                .build();
 
-		userRepository.save(user);
+        userRepository.save(user);
 
-		return Api.ok(userSignupReq.getUserLoginId() + " 회원가입 성공");
-	}
+        return Api.ok(userSignupReq.getUserLoginId() + " 회원가입 성공");
+    }
 
-	@Transactional
-	@Override
-	public Api<UserLoginRes> login(UserLoginReq userLoginReq) {
+    @Transactional
+    @Override
+    public Api<UserLoginRes> login(UserLoginReq userLoginReq) {
 
-		if (!isValidEmail(userLoginReq.getUserLoginId())) {
-			throw new ApiException(UserErrorCode.INVALID_EMAIL);
-		}
+        if (!isValidEmail(userLoginReq.getUserLoginId())) {
+            throw new ApiException(UserErrorCode.INVALID_EMAIL);
+        }
 
-		if (!isValidPassword(userLoginReq.getUserPw())) {
-			throw new ApiException(UserErrorCode.INVALID_PASSWORD);
-		}
+        if (!isValidPassword(userLoginReq.getUserPw())) {
+            throw new ApiException(UserErrorCode.INVALID_PASSWORD);
+        }
 
-		User user = userRepository.findByUserLoginId(userLoginReq.getUserLoginId())
-			.orElseThrow(() -> new ApiException(UserErrorCode.USER_NOT_FOUND));
+        User user = userRepository.findByUserLoginId(userLoginReq.getUserLoginId())
+                .orElseThrow(() -> new ApiException(UserErrorCode.USER_NOT_FOUND));
 
-		if (!passwordEncoder.matches(userLoginReq.getUserPw(), user.getPassword())) {
-			throw new ApiException(UserErrorCode.WRONG_PASSWORD);
-		}
+        if (!passwordEncoder.matches(userLoginReq.getUserPw(), user.getPassword())) {
+            throw new ApiException(UserErrorCode.WRONG_PASSWORD);
+        }
 
-		// Authentication 객체 생성
-		GrantedAuthority authority = new SimpleGrantedAuthority(user.getUserRole().getValue());
-		List<GrantedAuthority> authorities = Collections.singletonList(authority);
+        // Authentication 객체 생성
+        GrantedAuthority authority = new SimpleGrantedAuthority(user.getUserRole().getValue());
+        List<GrantedAuthority> authorities = Collections.singletonList(authority);
 
-		Authentication auth = new UsernamePasswordAuthenticationToken(
-			user.getUserLoginId(), user.getPassword(), authorities
-		);
+        Authentication auth = new UsernamePasswordAuthenticationToken(
+                user.getUserLoginId(), user.getPassword(), authorities
+        );
 
-		JwtToken jwtToken = jwtTokenProvider.generateToken(auth);
+        JwtToken jwtToken = jwtTokenProvider.generateToken(auth);
 
-		return Api.ok(new UserLoginRes(user.getUserLoginId(), user.getUserNickname(), user.getUserPicture(),
-			jwtToken.getAccessToken()));
+        return Api.ok(new UserLoginRes(user.getUserLoginId(), user.getUserNickname(), user.getUserPicture(),
+                jwtToken.getAccessToken()));
 
-		// return Api.ok(jwtToken.getAccessToken()); // UserLoginResponseDto 로 보내기
-	}
+        // return Api.ok(jwtToken.getAccessToken()); // UserLoginResponseDto 로 보내기
+    }
 
-	@Transactional
-	@Override
-	public Api<UserUpdateRes> updateByUserId(Long userId, UserUpdateReq userUpdateReq) {
-		User user = userRepository.findByUserId(userId)
-			.orElseThrow(() -> new ApiException(UserErrorCode.USER_NOT_FOUND));
+    @Transactional
+    @Override
+    public Api<UserUpdateRes> updateByUserNickname(String userNickname, UserUpdateReq userUpdateReq) {
+        User user = userRepository.findByUserNickname(userNickname)
+                .orElseThrow(() -> new ApiException(UserErrorCode.USER_NOT_FOUND));
 
-		user.updateUser(
-			userUpdateReq.getUserNickname(),
-			userUpdateReq.getUserPhone(),
-			userUpdateReq.getUserPicture(),
-			userUpdateReq.getUserAboutMe(),
-			userUpdateReq.getUserGender(),
-			userUpdateReq.getUserLatitude(),
-			userUpdateReq.getUserLatitude(),
-			userUpdateReq.getUserAddress()
-		);
+        user.updateUser(
+                userUpdateReq.getUserNickname(),
+                userUpdateReq.getUserPhone(),
+                userUpdateReq.getUserPicture(),
+                userUpdateReq.getUserAboutMe(),
+                userUpdateReq.getUserGender(),
+                userUpdateReq.getUserLatitude(),
+                userUpdateReq.getUserLatitude(),
+                userUpdateReq.getUserAddress()
+        );
 
-		return Api.ok(user.toUserUpdateRes());
-	}
+        return Api.ok(user.toUserUpdateRes());
+    }
 
-	// 이메일 형식 검증 메소드
-	private boolean isValidEmail(String email) {
-		return EMAIL_PATTERN.matcher(email).matches();
-	}
+    @Transactional
+    @Override
+    public Api<UserReadRes> getByUserNickname(String userNickname) {
+        User user = userRepository.findUserByUserNickname(userNickname)
+                .orElseThrow(() -> new ApiException(UserErrorCode.USER_NOT_FOUND));
 
-	private boolean isValidPassword(String password) {
-		return PASSWORD_PATTERN.matcher(password).matches();
-	}
+        return Api.ok(user.toUserReadRes());
+    }
+
+    // 이메일 형식 검증 메소드
+    private boolean isValidEmail(String email) {
+        return EMAIL_PATTERN.matcher(email).matches();
+    }
+
+    private boolean isValidPassword(String password) {
+        return PASSWORD_PATTERN.matcher(password).matches();
+    }
 }
