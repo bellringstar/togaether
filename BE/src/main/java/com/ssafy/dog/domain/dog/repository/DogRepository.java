ac3f9d7f54803d9f1bd6d0edbc4bda3539d1a825
Phabala(Kim Gihong) <kihong2424@gmail.com>
2023-11-08 12:07:05 +0900

    [BE] Feat : 개 Read 기능 추가

--- BE/src/main/java/com/ssafy/dog/domain/dog/repository/DogRepository.java
+++ BE/src/main/java/com/ssafy/dog/domain/dog/repository/DogRepository.java
@@ -1,10 +1,13 @@
 package com.ssafy.dog.domain.dog.repository;
 
+import com.ssafy.dog.domain.dog.entity.Dog;
+import com.ssafy.dog.domain.user.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import com.ssafy.dog.domain.dog.entity.Dog;
+import java.util.List;
 
 @Repository
 public interface DogRepository extends JpaRepository<Dog, Long> {
+    List<Dog> findAllByUser(User user);
 }
--- BE/src/main/java/com/ssafy/dog/domain/dog/service/DogService.java
+++ BE/src/main/java/com/ssafy/dog/domain/dog/service/DogService.java
@@ -1,9 +1,15 @@
 package com.ssafy.dog.domain.dog.service;
 
+import com.ssafy.dog.common.api.Api;
 import com.ssafy.dog.domain.dog.dto.request.DogCreateReq;
+import com.ssafy.dog.domain.dog.dto.response.DogGetRes;
 import com.ssafy.dog.domain.dog.entity.Dog;
+
+import java.util.List;
 
 public interface DogService {
 
-	Dog create(DogCreateReq dogCreateReq);
+    Dog create(DogCreateReq dogCreateReq);
+
+    Api<List<DogGetRes>> getDogs(Long userId);
 }
