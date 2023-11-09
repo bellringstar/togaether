b83f2b1b638881edc1f7d5cfaa28841e6c80e0e9
김현종 <dw3510@naver.com>
2023-11-09 14:33:58 +0900

    [FE] feat : 사용자 이동에 따른 polyline 추가

--- FE/dog/app/src/main/java/com/dog/data/viewmodel/map/LocationTrackingViewModel.kt
+++ FE/dog/app/src/main/java/com/dog/data/viewmodel/map/LocationTrackingViewModel.kt
@@ -4,6 +4,7 @@ import android.annotation.SuppressLint
 import android.content.Context
 import android.os.Looper
 import android.util.Log
+import androidx.core.location.LocationManagerCompat.getCurrentLocation
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.google.android.gms.location.FusedLocationProviderClient
@@ -32,19 +33,6 @@ class LocationTrackingViewModel(
         Priority.PRIORITY_HIGH_ACCURACY, UPDATE_INTERVAL_IN_MILLISECONDS
     ).build()
 
-    private val locationCallback = object : LocationCallback() {
-        override fun onLocationResult(locationResult: LocationResult) {
-            locationResult.locations.let { locations ->
-                for (location in locations) {
-                    val newLatLng = LatLng(location.latitude, location.longitude)
-                    _userLocation.value = newLatLng
-                    _pathPoints.value.add(newLatLng)
-                    Log.d("LocationTracking", "New location added: $newLatLng")
-                }
-                Log.d("LocationTracking", "Current path points: ${_pathPoints.value}")
-            }
-        }
-    }
 
     init {
         getCurrentLocation()
@@ -53,14 +41,23 @@ class LocationTrackingViewModel(
     @SuppressLint("MissingPermission")
     fun getCurrentLocation() {
         fusedLocationClient.lastLocation.addOnSuccessListener { location ->
-            // 위치 정보가 성공적으로 가져와졌을 때
             location?.let {
                 val currentLatLng = LatLng(it.latitude, it.longitude)
                 _userLocation.value = currentLatLng
-                _pathPoints.value.add(currentLatLng)
-                Log.i("LocationTracking", "현재 위치: ${_userLocation.value}") // 로그를 여기로 옮깁니다.
+                addPathPoint(currentLatLng) // 상태 업데이트
+                Log.i("LocationTracking", "현재 위치: ${_userLocation.value}")
             } ?: run {
                 startLocationUpdates()
+            }
+        }
+    }
+
+    private val locationCallback = object : LocationCallback() {
+        override fun onLocationResult(locationResult: LocationResult) {
+            locationResult.locations.forEach { location ->
+                val newLatLng = LatLng(location.latitude, location.longitude)
+                _userLocation.value = newLatLng
+                addPathPoint(newLatLng) // 상태 업데이트
             }
         }
     }
@@ -77,7 +74,14 @@ class LocationTrackingViewModel(
         }
     }
 
+    fun addPathPoint(newPoint: LatLng) {
+        val currentList = _pathPoints.value.toMutableList()
+        currentList.add(newPoint)
+        _pathPoints.value = currentList
+    }
+
     fun startTracking() {
+        resetPathPoints()
         _userLocation.value?.let {
             startLocationUpdates()
         } ?: run {
@@ -85,12 +89,18 @@ class LocationTrackingViewModel(
         }
     }
 
+
     fun stopTracking() {
         viewModelScope.launch {
             fusedLocationClient.removeLocationUpdates(locationCallback)
             Log.i("LocationTracking", "현재 위치: ${_userLocation.value} 종료")
-
+            resetPathPoints()
         }
+    }
+
+    fun resetPathPoints() {
+        _pathPoints.value.clear()
+        Log.i("LocationTracking", "Path points have been reset.")
     }
 
 }
