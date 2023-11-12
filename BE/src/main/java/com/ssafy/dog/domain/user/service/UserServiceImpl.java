078993972121230317d6bda1980e83da5e8d12b9
Phabala(Kim Gihong) <kihong2424@gmail.com>
2023-11-12 14:57:18 +0900

    [BE] Chore : 유저 생성시 기본 프로필 이미지 자동 저장
    이미지 링크 : "https://mblogthumb-phinf.pstatic.net/MjAyMDA2MTBfMTY1/MDAxNTkxNzQ2ODcyOTI2.Yw5WjjU3IuItPtqbegrIBJr3TSDMd_OPhQ2Nw-0-0ksg.8WgVjtB0fy0RCv0XhhUOOWt90Kz_394Zzb6xPjG6I8gg.PNG.lamute/user.png?type=w800"

--- BE/src/main/java/com/ssafy/dog/domain/user/service/UserServiceImpl.java
+++ BE/src/main/java/com/ssafy/dog/domain/user/service/UserServiceImpl.java
@@ -61,6 +61,7 @@ public class UserServiceImpl implements UserService {
                 .withUserTermsAgreed(userSignupReq.getUserTermsAgreed())
                 .withUserIsRemoved(false)
                 .withUserRole(UserRole.USER)
+                .withUserPicture("https://mblogthumb-phinf.pstatic.net/MjAyMDA2MTBfMTY1/MDAxNTkxNzQ2ODcyOTI2.Yw5WjjU3IuItPtqbegrIBJr3TSDMd_OPhQ2Nw-0-0ksg.8WgVjtB0fy0RCv0XhhUOOWt90Kz_394Zzb6xPjG6I8gg.PNG.lamute/user.png?type=w800")
                 .build();
 
         userRepository.save(user);
