ce1ede1f782364eeea3284d3a0eba1869d1cf39e
김현종 <dw3510@naver.com>
2023-11-13 17:25:05 +0900

    [FE] feat : 댓글 추가

--- FE/dog/app/src/main/java/com/dog/data/viewmodel/feed/HomeViewModel.kt
+++ FE/dog/app/src/main/java/com/dog/data/viewmodel/feed/HomeViewModel.kt
@@ -15,6 +15,7 @@ import com.dog.data.model.feed.BoardItem
 import com.dog.data.model.feed.BoardResponse
 import com.dog.data.repository.CommentRepository
 import com.dog.data.repository.FeedRepository
+import com.dog.data.repository.GpsRepository
 import com.dog.util.common.DataStoreManager
 import com.dog.util.common.RetrofitClient
 import dagger.hilt.android.lifecycle.HiltViewModel
@@ -29,6 +30,8 @@ class HomeViewModel @Inject constructor(
     private val dataStoreManager: DataStoreManager
 ) : ViewModel() {
     private val interceptor = RetrofitClient.RequestInterceptor(dataStoreManager)
+    private val addCommentApi: CommentRepository =
+        RetrofitClient.getInstance(interceptor).create(CommentRepository::class.java)
     private val _feedList = mutableStateListOf<BoardItem>()
     val feedListState: SnapshotStateList<BoardItem> get() = _feedList
 
@@ -55,41 +58,42 @@ class HomeViewModel @Inject constructor(
         }
     }
 
-
-    suspend fun addComment(addCommentRequest: AddCommentRequest) {
-        Log.d("add", addCommentRequest.toString())
-        try {
-            val AddCommentApi: CommentRepository =
-                RetrofitClient.getInstance(interceptor).create(CommentRepository::class.java)
-            val response = AddCommentApi.addCommentApiResponse(addCommentRequest)
-            if (response.isSuccessful) {
-                val commentResponse = response.body()
-                commentResponse?.let {
-                    // 성공적으로 댓글이 추가되었을 때의 처리
-                    val newCommentItem = CommentItem(
-                        boardId = addCommentRequest.boardId,
-                        commentId = 0,
-                        commentContent = addCommentRequest.commentContent,
-                        userNickname = addCommentRequest.userNickname,
-                        commentLikes = 0,
-                        userProfileUrl = ""  // 여기에 프로필 URL을 설정해야 합니다.
+    fun addComment(addCommentRequest: AddCommentRequest) {
+        viewModelScope.launch {
+            Log.d("API_Response", addCommentRequest.toString())
+            try {
+                val response = addCommentApi.addCommentApiResponse(addCommentRequest)
+                if (response.isSuccessful) {
+                    Log.i("API_Response", "댓글 등록 : ${response.body()}")
+                    val commentResponse = response.body()
+                    commentResponse?.let {
+                        // 성공적으로 댓글이 추가되었을 때의 처리
+                        val newCommentItem = CommentItem(
+                            boardId = addCommentRequest.boardId,
+                            commentId = 0,
+                            commentContent = addCommentRequest.commentContent,
+                            userNickname = addCommentRequest.userNickname,
+                            commentLikes = 0,
+                            userProfileUrl = ""  // 여기에 프로필 URL을 설정해야 합니다.
+                        )
+                        _commentList.add(newCommentItem)
+                        _commentsCount.value = _commentList.size
+                    }
+                } else {
+                    // 댓글 추가 실패 시의 처리
+                    Log.e(
+                        "API_Response",
+                        "Add Comment Failed: ${response.code()}, ${response.message()}"
                     )
-                    _commentList.add(newCommentItem)
-                    _commentsCount.value = _commentList.size
                 }
-            } else {
-                // 댓글 추가 실패 시의 처리
-                Log.e(
-                    "API_Response",
-                    "Add Comment Failed: ${response.code()}, ${response.message()}"
-                )
+            } catch (e: Exception) {
+                // 예외 발생 시의 처리
+                // 만약 빈 배열이 들어올 때 예외를 무시하고 싶다면 여기에 추가적인 로그만 남기거나 아무 작업을 하지 않으면 됩니다.
+                Log.e("API_Response", "Exception: ${e.message}", e)
             }
-        } catch (e: Exception) {
-            // 예외 발생 시의 처리
-            // 만약 빈 배열이 들어올 때 예외를 무시하고 싶다면 여기에 추가적인 로그만 남기거나 아무 작업을 하지 않으면 됩니다.
-            Log.e("API_Response", "Exception: ${e.message}", e)
         }
     }
+
 
     suspend fun loadCommentListData(
         boardId: Long
