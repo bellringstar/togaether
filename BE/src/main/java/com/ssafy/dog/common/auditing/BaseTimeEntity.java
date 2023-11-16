1191b7b621e84cce094677c483db0c2743c4adc3
김대홍 <rlaeoghd1885@naver.com>
2023-11-16 13:50:03 +0900

    [BE] Refactor : BaseEntity 에서 timezone 설정

--- BE/src/main/java/com/ssafy/dog/common/auditing/BaseTimeEntity.java
+++ BE/src/main/java/com/ssafy/dog/common/auditing/BaseTimeEntity.java
@@ -1,21 +1,31 @@
 package com.ssafy.dog.common.auditing;
 
 import java.time.LocalDateTime;
+import java.time.ZoneId;
+
 import javax.persistence.EntityListeners;
 import javax.persistence.MappedSuperclass;
-import lombok.Getter;
+
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import lombok.Getter;
 
 @Getter
 @MappedSuperclass
 @EntityListeners(AuditingEntityListener.class)
 public abstract class BaseTimeEntity {
 
-    @CreatedDate
-    private LocalDateTime createdDate;
+	@CreatedDate
+	private LocalDateTime createdDate;
 
-    @LastModifiedDate
-    private LocalDateTime modifiedDate;
+	@LastModifiedDate
+	private LocalDateTime modifiedDate;
+
+	// Constructor
+	public BaseTimeEntity() {
+		this.createdDate = LocalDateTime.now(ZoneId.of("Asia/Seoul"));
+		this.modifiedDate = LocalDateTime.now(ZoneId.of("Asia/Seoul"));
+	}
 }
